7f1921052dda0adf46ed24b99b6a95ac2de6d714
mihett05 <mihett05@yandex.ru>
2024-09-29 11:36:15 +0500

    Updated dataset processing

--- main.py
+++ main.py
@@ -1,35 +1,34 @@
 import asyncio
-
-import pandas as pd
+import sys
 
 from clustering import IncrementalStringRecordLinkage
 from comparison import classify, compare_rows
 from datasource import ClickhouseClient
 from preprocess import preprocess_table
 from preprocess.dataset1 import parse_dataset1_row
-from standard import DatasetRow
+from preprocess.dataset2 import parse_dataset2_row
+from preprocess.dataset3 import parse_dataset3_row
 
 
 async def main():
     client = await ClickhouseClient.create()
-    # await client.create_normalized_table()
-    # await client.create_results_table()
-    # print("Starting preprocessing")
-    # await preprocess_table(client, "table_dataset1", parse_dataset1_row)
-    # await preprocess_table(client, "table_dataset2", parse_dataset2_row)
-    # await preprocess_table(client, "table_dataset2", parse_dataset3_row)
-    # print("Finished processing")
+    await client.create_normalized_table()
+    await client.create_results_table()
+    print("Starting preprocessing")
+    await preprocess_table(client, "table_dataset1", parse_dataset1_row)
+    await preprocess_table(client, "table_dataset2", parse_dataset2_row)
+    await preprocess_table(client, "table_dataset2", parse_dataset3_row)
+    print("Finished processing")
 
-    rows = [DatasetRow(*row, source=1) for row in await client.query("select * from table_dataset1 LIMIT 1000000")]
-    print('rows obtained')
-
-    dataframe = pd.DataFrame(rows)
+    dataframe = await client.query_df("select * from normalized where source = 1 limit 1000000")
+    print("rows obtained")
+    print(dataframe)
 
     features = compare_rows(dataframe)
-    print('features obtained')
+    print("features obtained")
 
     matches = classify(features)
-    print('matches obtained')
+    print("matches obtained")
 
     incremental_cluster = IncrementalStringRecordLinkage()
 
@@ -41,11 +40,11 @@ async def main():
             extra_indices.remove(match[0])
         if match[1] in extra_indices:
             extra_indices.remove(match[1])
-    print('added pairs to clusters')
-    
+    print("added pairs to clusters")
+
     for index in extra_indices:
         incremental_cluster.add_record((dataframe.iloc[index]))
-    print('added extra indices to clusters')
+    print("added extra indices to clusters")
 
     # total_length = 0
     # for cluster_uids, cluster_items in incremental_cluster.clusters:
@@ -57,6 +56,9 @@ async def main():
 
 if __name__ == "__main__":
     from time import time
+
+    if sys.platform == "win32":
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
     start_time = time()
     asyncio.run(main())
